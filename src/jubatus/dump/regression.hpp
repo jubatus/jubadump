343460177571ca5d3de5852184083d4a5f31aef4
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2016-11-09 20:50:54 +0900

    support Jubatus 1.0 model files

--- src/jubatus/dump/regression.hpp
+++ src/jubatus/dump/regression.hpp
@@ -1,5 +1,5 @@
 // Jubatus: Online machine learning framework for distributed environment
-// Copyright (C) 2013 Preferred Networks and Nippon Telegraph and Telephone Corporation.
+// Copyright (C) 2016 Preferred Networks and Nippon Telegraph and Telephone Corporation.
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
@@ -14,8 +14,8 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-#ifndef JUBATUS_DUMP_CLASSIFIER_HPP_
-#define JUBATUS_DUMP_CLASSIFIER_HPP_
+#ifndef JUBATUS_DUMP_REGRESSION_HPP_
+#define JUBATUS_DUMP_REGRESSION_HPP_
 
 #include <stdint.h>
 
@@ -25,37 +25,34 @@
 #include <jubatus/util/data/serialization.h>
 
 #include "types.hpp"
-#include "local_storage_mixture.hpp"
 #include "weight_manager.hpp"
-#include "labels.hpp"
+#include "local_storage_mixture.hpp"
+#include "regression.hpp"
 
 namespace jubatus {
 namespace dump {
 
-struct linear_classifier {
+struct linear_regression {
   local_storage_mixture storage;
-  labels label_storage;
 
-  MSGPACK_DEFINE(storage, label_storage);
+  MSGPACK_DEFINE(storage);
 };
 
-struct linear_classifier_dump {
-  explicit linear_classifier_dump(const linear_classifier& classifier)
-      : storage(classifier.storage),
-        label(classifier.label_storage) {
+struct linear_regression_dump {
+  explicit linear_regression_dump(const linear_regression& regression)
+      : storage(regression.storage) {
   }
 
   local_storage_mixture_dump storage;
-  labels_dump label;
 
   template <class Ar>
   void serialize(Ar& ar) {
-    ar & JUBA_MEMBER(storage) & JUBA_MEMBER(label);
+    ar & JUBA_MEMBER(storage);
   }
 };
 
 template <typename S>
-struct classifier {
+struct regression {
   typedef S storage_type;
 
   storage_type storage;
@@ -65,13 +62,13 @@ struct classifier {
 };
 
 template <typename S, typename D>
-struct classifier_dump {
+struct regression_dump {
   typedef S storage_type;
   typedef D dump_type;
 
-  explicit classifier_dump(const classifier<S>& classifier)
-      : storage(classifier.storage),
-        weights(classifier.weights) {
+  explicit regression_dump(const regression<S>& regression)
+      : storage(regression.storage),
+        weights(regression.weights) {
   }
 
   dump_type storage;
@@ -86,4 +83,4 @@ struct classifier_dump {
 }  // namespace dump
 }  // namespace jubatus
 
-#endif  // JUBATUS_DUMP_CLASSIFIER_HPP_
+#endif  // JUBATUS_DUMP_REGRESSION_HPP_
